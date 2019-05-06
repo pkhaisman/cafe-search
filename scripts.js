6e94d60c8437d3afd77014710b2c5434cdee13e7
Philip Khaisman <pkhaisman@gmail.com>
2019-05-06 14:25:58 -0400

    allow search by current location or input location

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ function formatCafe(cafe) {
     console.log('formatCafe ran');
 
     return `
-        <li>${cafe}</li>
+        <li class="list-item">${cafe}</li>
     `;
 }
 
@@ -18,10 +18,13 @@ function renderCafes(searchResults) {
     })
 }
 
-function fetchCafes(location) {
+function fetchCafes(coords) {
     console.log('fetchCafes ran');
 
-    const url = 'https://developers.zomato.com/api/v2.1/search?entity_id=287&entity_type=city&establishment_type=286';
+    const lat = coords.lat;
+    const long = coords.long;
+
+    const url = `https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${long}&radius=8000&establishment_type=286`;
     const apiKey = 'bf3a734186bb3b42c6dfa0df0eb323f5';
     const options =  {
         headers: new Headers({
@@ -34,19 +37,62 @@ function fetchCafes(location) {
         .then(responseJson => renderCafes(responseJson));
 }
 
-function handleSearch() {
-    console.log('handleSearch ran');
+function handleGeolocationSearch() {
+    console.log('handleGeolocationSearch ran');
+    if ('geolocation' in navigator) {
+        console.log('geolocation in navigator');
+        navigator.geolocation.getCurrentPosition(position => {
+            const coords = {
+                lat: position.coords.latitude,
+                long: position.coords.longitude
+            }        
+            fetchCafes(coords);
+        });
+    } else {
+        console.log('prompt user to input location');
+    }
+}
+
+function convertToLatLong(responseJson) {
+    console.log('convertToLatLong ran');
+    const coords = {
+        lat: responseJson.results[0].geometry.location.lat,
+        long: responseJson.results[0].geometry.location.lng
+    }
+    fetchCafes(coords);
+}
+
+function fetchLocation(location) {
+    console.log('fetchLocation ran');
+
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyAkVfjrsbDT63PPJiJ10m3lNEiEy6Yjhao`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(responseJson => convertToLatLong(responseJson));
+}
+
+function handleLocationSearch() {
+    console.log('handleLocationSearch ran');
 
     let location = $('.cafe-search__input').val();
-    fetchCafes(location);
+    console.log(location);
+    fetchLocation(location);
 }
 
 function submitClickListener() {
     console.log('submitClickListener ran');
 
-    $('.cafe-search__form').submit(event => {
+    $('.cafe-search__location-form').submit(event => {
+        console.log('click location');
         event.preventDefault();
-        handleSearch();
+        handleLocationSearch();
+    })
+
+    $('.cafe-search__geolocation-form').submit(event => {
+        console.log('click geolocation');
+        event.preventDefault();
+        handleGeolocationSearch();
     })
 }
 
